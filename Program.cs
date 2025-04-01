060708a016a9ddc50b05090dfba43efa6b9daf99
Mikhail <zaqzxcswsde@gmail.com>
2025-04-01 18:25:51 +0300

    add creation of .env file

--- Program.cs
+++ Program.cs
@@ -12,6 +12,19 @@ namespace diplomaadminpanel
         [STAThread]
         static void Main()
         {
+
+            string envPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ".env");
+
+            if (!File.Exists(envPath))
+            {
+                File.WriteAllLines(envPath, new string[]
+                {
+                    "ADMIN_TOKEN_DEV=",
+                    "ADMIN_TOKEN_PROD="
+                });
+            }
+
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             //DotEnv.Load(options: new DotEnvOptions(probeForEnv: true, probeLevelsToSearch: 2));
