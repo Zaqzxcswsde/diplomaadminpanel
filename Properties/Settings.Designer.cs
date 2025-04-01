51f19fe3a2c8391c41d9a09477a21d106c580d7b
Mikhail Ivanov <zaqzxcswsde@gmail.com>
2025-04-01 18:30:42 +0300

    default env to prod

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace diplomaadminpanel.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("prod")]
         public string env {
             get {
                 return ((string)(this["env"]));
