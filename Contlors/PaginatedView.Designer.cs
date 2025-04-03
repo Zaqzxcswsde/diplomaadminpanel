8a9c48a5a1e0d3ae18d8847244aa398f9280f73f
Mikhail Ivanov <zaqzxcswsde@gmail.com>
2025-04-03 12:53:14 +0300

    change scaling to dpi

--- Contlors/PaginatedView.Designer.cs
+++ Contlors/PaginatedView.Designer.cs
@@ -40,10 +40,10 @@
             // lblPage
             // 
             lblPage.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            lblPage.Location = new Point(53, 0);
+            lblPage.Location = new Point(35, 0);
             lblPage.Margin = new Padding(0);
             lblPage.Name = "lblPage";
-            lblPage.Size = new Size(64, 42);
+            lblPage.Size = new Size(43, 28);
             lblPage.TabIndex = 18;
             lblPage.Text = "? / ?";
             lblPage.TextAlign = ContentAlignment.MiddleCenter;
@@ -52,10 +52,10 @@
             // 
             btnNext.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             btnNext.Enabled = false;
-            btnNext.Location = new Point(119, 0);
+            btnNext.Location = new Point(79, 0);
             btnNext.Margin = new Padding(0);
             btnNext.Name = "btnNext";
-            btnNext.Size = new Size(50, 42);
+            btnNext.Size = new Size(33, 28);
             btnNext.TabIndex = 17;
             btnNext.Text = "->";
             btnNext.UseVisualStyleBackColor = true;
@@ -64,10 +64,10 @@
             // 
             btnPrev.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             btnPrev.Enabled = false;
-            btnPrev.Location = new Point(3, 0);
+            btnPrev.Location = new Point(2, 0);
             btnPrev.Margin = new Padding(0);
             btnPrev.Name = "btnPrev";
-            btnPrev.Size = new Size(50, 42);
+            btnPrev.Size = new Size(33, 28);
             btnPrev.TabIndex = 16;
             btnPrev.Text = "<-";
             btnPrev.UseVisualStyleBackColor = true;
@@ -81,7 +81,7 @@
             dgvView.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
             dgvView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvView.EditMode = DataGridViewEditMode.EditProgrammatically;
-            dgvView.Location = new Point(0, 50);
+            dgvView.Location = new Point(0, 33);
             dgvView.Margin = new Padding(0);
             dgvView.MultiSelect = false;
             dgvView.Name = "dgvView";
@@ -89,7 +89,7 @@
             dgvView.RowHeadersVisible = false;
             dgvView.RowHeadersWidth = 62;
             dgvView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvView.Size = new Size(714, 450);
+            dgvView.Size = new Size(476, 300);
             dgvView.TabIndex = 15;
             dgvView.CellContentClick += dgvView_CellContentClick;
             dgvView.CellDoubleClick += dgvView_CellDoubleClick;
@@ -100,21 +100,20 @@
             pnlBtns.Controls.Add(btnNext);
             pnlBtns.Controls.Add(lblPage);
             pnlBtns.Controls.Add(btnPrev);
-            pnlBtns.Location = new Point(546, 0);
+            pnlBtns.Location = new Point(364, 0);
             pnlBtns.Margin = new Padding(0);
             pnlBtns.Name = "pnlBtns";
-            pnlBtns.Size = new Size(169, 50);
+            pnlBtns.Size = new Size(113, 33);
             pnlBtns.TabIndex = 19;
             // 
             // PaginatedView
             // 
-            AutoScaleDimensions = new SizeF(144F, 144F);
+            AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
             Controls.Add(pnlBtns);
             Controls.Add(dgvView);
-            Margin = new Padding(4, 5, 4, 5);
             Name = "PaginatedView";
-            Size = new Size(714, 500);
+            Size = new Size(476, 333);
             Load += PaginatedView_Load;
             ((System.ComponentModel.ISupportInitialize)dgvView).EndInit();
             pnlBtns.ResumeLayout(false);
